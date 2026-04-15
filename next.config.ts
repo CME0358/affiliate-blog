470d63ecfcd00bac83f3f0681f28d81af3a602cb
CME0358 <engineer@coas.asia>
2026-04-15 10:10:25 +0900

    fix: outputFileTracingIncludesをexperimental内に修正

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/sitemap.xml": ["./content/posts/**/*"],
+  experimental: {
+    outputFileTracingIncludes: {
+      "/sitemap.xml": ["./content/posts/**/*"],
+    },
   },
 };
 
